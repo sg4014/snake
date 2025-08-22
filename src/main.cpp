49ddfe7f6eca60c48517c3e4ab4b0c7b0aec7c94
Sergey Georgiev <sergeygeorgiev1709@gmail.com>
2025-08-22 16:10:40 +0300

    add periodic movement on grid

--- src/main.cpp
+++ src/main.cpp
@@ -3,88 +3,116 @@
 #include <chrono>
 #include <iostream>
 #include <stdexcept>
+#include <array>
+
+namespace Grid
+{
+    constexpr int cellSide{ 40 };
+    constexpr int rows{ 15 };
+    constexpr int columns{ 20 };
+    constexpr int width{ cellSide * columns };
+    constexpr int height{ cellSide * rows };
+
+    auto window{ sf::RenderWindow(sf::VideoMode({ width, height }), "Snake") };
+
+    void drawGridLines()
+    {
+        // draw vertical lines
+        for (int col{ 1 }; col < columns; ++col)
+        {
+            const float x{ static_cast<float>(col * cellSide) };
+            std::array  line{
+                sf::Vertex{ { x, 0 } },
+                sf::Vertex{ { x, height } }
+            };
+            window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
+        }
+
+        // draw horizontal lines
+        for (int row{ 1 }; row < rows; ++row)
+        {
+            const float y{ static_cast<float>(row * cellSide) };
+            std::array  line{
+                sf::Vertex{ { 0, y } },
+                sf::Vertex{ { width, y } }
+            };
+            window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
+        }
+    }
+}
 
 class Snake
 {
 public:
-    enum Direction
+    enum MoveDirection
     {
-        top, bottom, left, right,
+        right, top, left, bottom,
     };
 
-    explicit Snake(float speed = 50, sf::Vector2f position = { 0, 0 })
-        : m_velocity{ sf::Vector2f{0.f, -speed}}
+    explicit Snake(sf::Vector2f position)
     {
-        m_shape.setOrigin(m_shape.getGeometricCenter());
-        m_shape.setPosition(position);
         m_shape.setFillColor(sf::Color::Red);
+        m_shape.setPosition(position);
     }
 
-    void move(Clock::Seconds deltaTime)
+    void drawIn(sf::RenderWindow& window) const
     {
-        m_shape.setPosition(m_shape.getPosition() + deltaTime.count() * m_velocity);
+        window.draw(m_shape);
     }
 
-    sf::Vector2f getPosition() const { return m_shape.getPosition(); }
+    void setMoveDirection(MoveDirection dir) { m_moveDirection = dir; }
 
-    void drawIn(sf::RenderWindow& window) const { window.draw(m_shape); }
-
-    void setDirection(Direction direction)
+    void move()
     {
-        if (direction != m_direction)
+        sf::Vector2f delta{};
+
+        switch (m_moveDirection)
         {
-            m_direction = direction;
-            m_velocity = getUpdatedVelocity();
+        case top:
+            delta = { 0, -Grid::cellSide };
+            break;
+        case bottom:
+            delta = { 0, Grid::cellSide };
+            break;
+        case left:
+            delta = { -Grid::cellSide, 0 };
+            break;
+        case right:
+            delta = { Grid::cellSide, 0 };
+            break;
+        default:
+            throw std::runtime_error("Unknown enumerator of MoveDirection");
         }
+
+        m_shape.setPosition(m_shape.getPosition() + delta);
     }
 
 private:
-    sf::RectangleShape m_shape{ { 40, 40 } };
-    Direction          m_direction{};
-    sf::Vector2f       m_velocity{};
-
-    sf::Vector2f getUpdatedVelocity() const
-    {
-        switch (m_direction)
-        {
-        case top:    return { 0.f, -m_velocity.length() };
-        case bottom: return { 0.f, m_velocity.length() };
-        case left:   return { -m_velocity.length(), 0.f };
-        case right:  return { m_velocity.length(), 0.f };
-        default:
-            throw std::out_of_range(
-                "Impossible enumerator encountered."
-                " Only 'top', 'bottom', 'left', 'right' are possible."
-            );
-        }
-    }
+    MoveDirection      m_moveDirection{ right };
+    sf::RectangleShape m_shape{ { Grid::cellSide, Grid::cellSide } };
 };
-
-void update(Clock::Seconds dt, Snake& snake);
 
 int main()
 {
-    constexpr int WIDTH{ 600 };
-    constexpr int HEIGHT{ 400 };
-
-    auto window = sf::RenderWindow(sf::VideoMode({ WIDTH, HEIGHT }), "Snake");
-
-    // Snake
-    constexpr float speed{ 120.f }; // pixels per second
-    Snake           snake{ speed, { WIDTH / 2.f, HEIGHT / 2.f } };
-
     // Clock
     Clock clock{};
     clock.start();
 
+    constexpr sf::Vector2f center{
+        Grid::cellSide * (Grid::columns / 2),
+        Grid::cellSide * (Grid::rows / 2)
+    };
+
+    Snake snake{ center };
+
     // Game Loop
-    while (window.isOpen())
+    while (Grid::window.isOpen())
     {
-        while (const std::optional event{ window.pollEvent() })
+        while (const std::optional event{ Grid::window.pollEvent() })
         {
             if (event->is<sf::Event::Closed>())
             {
-                window.close();
+                Grid::window.close();
             }
             else if (const auto* keyPressed{ event->getIf<sf::Event::KeyPressed>() })
             {
@@ -93,36 +121,41 @@ int main()
                 switch (keyPressed->scancode)
                 {
                 case Escape:
-                    window.close();
+                    Grid::window.close();
                     break;
                 case W:
-                    snake.setDirection(Snake::top);
+                    snake.setMoveDirection(Snake::top);
                     break;
                 case A:
-                    snake.setDirection(Snake::left);
+                    snake.setMoveDirection(Snake::left);
                     break;
                 case S:
-                    snake.setDirection(Snake::bottom);
+                    snake.setMoveDirection(Snake::bottom);
                     break;
                 case D:
-                    snake.setDirection(Snake::right);
+                    snake.setMoveDirection(Snake::right);
                     break;
-                default:;
+                default: ;
                 }
             }
         }
 
-        // Clear and Update
-        window.clear(sf::Color::Black);
-        update(clock.reset(), snake);
+        // update screen each s seconds =============
+        Clock::Seconds period {0.2};
 
-        // Draw and display
-        snake.drawIn(window);
-        window.display();
+        if (clock.elapsed() < period)
+            continue;
+
+        Grid::window.clear(sf::Color::Black);
+
+        snake.move();
+        snake.drawIn(Grid::window);
+
+        Grid::drawGridLines();
+
+        Grid::window.display();
+
+        clock.reset();
+        // ==========================================
     }
-}
-
-void update(Clock::Seconds dt, Snake& snake)
-{
-    snake.move(dt);
 }
